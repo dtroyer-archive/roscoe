b1efea7fbb9c1f3290debadbf18aa0bb8f2ee9a5
Dean Troyer <dtroyer@gmail.com>
2012-10-06 02:30:32 -0500

    Add NewRequest(), NewResponse(), Do(), Get() to Client

--- client/client.go
+++ client/client.go
@@ -6,6 +6,7 @@ import (
     "bytes"
     "encoding/json"
 //    "fmt"
+    "io"
     "io/ioutil"
     "net/http"
     "net/http/httputil"
@@ -13,12 +14,8 @@ import (
     "roscoe/osclib"
 )
 
-type Client struct {
-    httpClient *http.Client
-    Auth osclib.Creds
-    Token Token
-    ServCat []ServiceCatalogEntry
-}
+
+// Identity Types
 
 type IdentTokens struct {
     Access struct {
@@ -43,6 +40,23 @@ type ServiceCatalogEntry struct {
     Endpoints []map[string]string
 }
 
+type ServiceEndpoint struct {
+    Type string
+    Region string
+    URL string
+    VersionId string
+}
+
+
+// Client
+
+type Client struct {
+    httpClient *http.Client
+    Auth osclib.Creds
+    Token Token
+    ServCat map[string]ServiceEndpoint
+}
+
 func NewClient(creds osclib.Creds) (oscc *Client, err error) {
     // Get credentials
     if creds.OSAuth.PasswordCredentials.Username == "" {
@@ -56,6 +70,38 @@ func NewClient(creds osclib.Creds) (oscc *Client, err error) {
         Auth: creds,
     }
     return oscc, nil
+}
+
+func (c *Client) NewRequest(method, url string, body io.Reader) (req *http.Request, err error) {
+    req, err = http.NewRequest(method, url, body)
+    if err != nil {
+		return
+	}
+	// add token, get one if needed
+	if c.Token.Id == "" {
+        err = c.Authenticate()
+        if err != nil {
+            return nil, err
+        }
+    }
+    req.Header.Add("X-Auth-Token", c.Token.Id)
+	return
+}
+
+func (c *Client) Do(req *http.Request) (res *Result, err error) {
+    // TODO(dtroyer): work out debug semantics
+    if 1 == 0 {
+        d, _ := httputil.DumpRequestOut(req, true)
+        print("req: ", string(d), "\n\n")
+    }
+
+	resp, err := c.httpClient.Do(req)
+	if err != nil {
+		return
+	}
+
+	res = NewResult(resp)
+	return
 }
 
 // Authenticate to Identity service
@@ -72,8 +118,32 @@ func (c *Client) Authenticate() (err error) {
     return err
 }
 
+// Perform a simple get
+func (c *Client) Get(api string, url string) (resp *Result, err error) {
+    req, err := c.NewRequest("GET", c.ServCat[api].URL + url, nil)
+    if err != nil {
+		return nil, err
+    }
+
+    resp, err = c.Do(req)
+    if err != nil {
+		return nil, err
+    }
+    // do we need to parse this in this func? yes...
+    defer resp.HResponse.Body.Close()
+
+    resp.Body, err = ioutil.ReadAll(resp.HResponse.Body)
+    if err != nil {
+		return nil, err
+    }
+
+    return resp, nil
+}
+
 func (c *Client) getToken() (err error) {
     // Build the request body
+    // Call the http method directly to avoid a recursive call
+    // back here if no current token is held
     req, err := http.NewRequest(
         "POST",
         c.Auth.AuthUrl + "/tokens",
@@ -85,26 +155,21 @@ func (c *Client) getToken() (err error) {
 
     req.Header.Add("content-type", "application/json")
 
-    // TODO(dtroyer): work out debug semantics
-    if 1 == 0 {
-        d, _ := httputil.DumpRequestOut(req, true)
-        print("req: ", string(d), "\n\n")
-    }
 
-    resp, err := c.httpClient.Do(req)
+    resp, err := c.Do(req)
     if err != nil {
         // TODO(dtroyer): Handle specific errors
         return err
     }
-    defer resp.Body.Close()
+    defer resp.HResponse.Body.Close()
 
     // TODO(dtroyer): work out debug semantics
     if 1 == 0 {
-        dr, _ := httputil.DumpResponse(resp, true)
+        dr, _ := httputil.DumpResponse(resp.HResponse, true)
         print("resp: ", string(dr), "\n\n")
     }
 
-    contents, err := ioutil.ReadAll(resp.Body)
+    contents, err := ioutil.ReadAll(resp.HResponse.Body)
     if err != nil {
         return err
     }
@@ -116,7 +181,36 @@ func (c *Client) getToken() (err error) {
     }
 
     c.Token = access.Access.Token
-    c.ServCat = access.Access.ServiceCatalog
+
+    // Parse service catalog
+    c.ServCat = map[string]ServiceEndpoint{}
+    for _, v := range access.Access.ServiceCatalog {
+        // we only look at the first endpoint for each type
+        c.ServCat[v.Type] = ServiceEndpoint{
+            Type: v.Type,
+            Region: v.Endpoints[0]["region"],
+            URL: v.Endpoints[0]["publicURL"],
+            VersionId: v.Endpoints[0]["versionId"],
+        }
+    }
+
     return nil
 }
 
+
+// Result
+
+type Result struct {
+    HResponse *http.Response
+    Body []byte
+}
+
+func NewResult(resp *http.Response) *Result {
+	result := new(Result)
+	result.HResponse = resp
+
+	//result.parseHeader()
+	//result.parseBody()
+
+	return result
+}
--- osc.go
+++ osc.go
@@ -3,12 +3,19 @@
 package main
 
 import (
+    "encoding/json"
     "fmt"
     "log"
 
     "roscoe/client"
     "roscoe/osclib"
+//    "roscoe/server"
 )
+
+
+type ServerResponse struct {
+    Servers []interface{}
+}
 
 func main() {
     // Get auth values from the environment
@@ -18,9 +25,25 @@ func main() {
         log.Fatal(err)
     }
 
+    // Identity API versions
     osclib.GetVersions(c.Auth)
 
+    // Test authentication
     c.Authenticate()
-    fmt.Printf("token: %s\n", c.Token)
-    fmt.Printf("servcat: %s\n", c.ServCat)
+//    fmt.Printf("token: %s\n", c.Token)
+//    fmt.Printf("servcat: %s\n", c.ServCat)
+
+    // list servers
+    resp, err := c.Get("compute", "/servers")
+    if err != nil {
+        // TODO(dtroyer): Handle specific errors
+		log.Fatal(err)
+    }
+    var servers ServerResponse
+    err = json.Unmarshal(resp.Body, &servers)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    fmt.Printf("c: %+v\n\n", servers)
 }
